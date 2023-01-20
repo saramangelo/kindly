c13de0fb330ee0d1e9363bcd1c3656ae589f0a68
edmond buck <edmond.buck@wearetissue.com>
2023-01-20 11:27:30 -0800

    updated oppurtunity modle

--- models/Opportunity.js
+++ models/Opportunity.js
@@ -27,6 +27,20 @@ Opportunity.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    items: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    volunteers_needed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    // This will save an image to database
+    photo: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+  
     user_id: {
       type: DataTypes.INTEGER,
       references: {
