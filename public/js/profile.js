c74d3bf5cd498da37ec5bf4bf3521116a7524795
saramangelo <saramangelo@gmail.com>
2023-01-21 14:43:32 -0800

    updated profile.js, changed back to the way it was

--- public/js/profile.js
+++ public/js/profile.js
@@ -37,7 +37,7 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/new-opportunity');
+      document.location.replace('/profile');
     } else {
       console.log(response)
       alert('Failed to create project');
