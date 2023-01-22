9abeb52d74d239a71c4e83cc600d7dd523d9f7d3
saramangelo <saramangelo@gmail.com>
2023-01-21 23:05:03 -0800

    removed erroneous packages

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,3 @@
-// const cldInstance = new Cloudinary({cloud: {cloudName: 'di3e0booq'}});
 // FRONT END - Creating opportunity on front end here, sending to the back end
 // This corresponds with opportunityRoutes.js on BE
 
@@ -65,28 +64,6 @@ const newFormHandler = async (event) => {
 //     }
 //   }
 // };
-
-
-// cloudinary widget
-
-// let myWidget = cloudinary.createUploadWidget(
-//   {
-//     cloudName: "di3e0booq",
-//     uploadPreset: "btw7ntpw"
-//   },
-//   (error, result) => {
-//     if (!error && result && result.event === "success"){
-//       console.log("Done! Here is the image info: ", result.info);
-//     }
-//   }
-// );
-
-// document.getElementById('#upload_widget').addEventListener('click', function(){
-//   myWidget.open();
-// },
-// false
-// );
-
 
 
 
