ccd132dd5a8dd85bc93a3280a19cee722db22e63
saramangelo <saramangelo@gmail.com>
2023-01-22 23:26:21 -0800

    removed iso method from profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -14,15 +14,6 @@ const newFormHandler = async (event) => {
   const items = document.querySelector('#items-to-bring').value.trim();
   const volunteers = document.querySelector('#volunteers-needed').value.trim();
 
-  // ISO to String Method for date
-  function createUTCdateForISO(date) {
-    const offset = new Date().getTimezoneOffset();
-    const myDate = Date.parse(date) - offset + 60 + 1000;
-    const dateAsISO = new Date(myDate).toISOString();
-    console.log(dateAsISO);
-    return dateAsISO;
-  }
-
   if (
     name &&
     sponsor &&
