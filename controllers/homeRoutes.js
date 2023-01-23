3cd644b48e4e81b75e6026cf38a32263a0dff39e
saramangelo <saramangelo@gmail.com>
2023-01-22 23:47:43 -0800

    removed comments on homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -23,9 +23,7 @@ router.get('/', async (req, res) => {
       opportunities,
       logged_in: req.session.logged_in,
     });
-    console.log(req.body)
   } catch (err) {
-    console.log(response)
     res.status(500).json(err);
   }
 });
@@ -87,69 +85,3 @@ router.get('/login', (req, res) => {
 });
 
 module.exports = router;
-
-/*
-
-    // Get all projects and JOIN with user data
-    const opportunityData = await Opportunity.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const opportunities = opportunityData.map((opportunity) => opportunity.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      opportunities,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/opportunities/:id', async (req, res) => {
-  try {
-    const opportunityData = await Opportunity.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const opportunity = opportunityData.get({ plain: true });
-
-    res.render('opportunity', {
-      ...opportunity,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-*/
-
-// Use withAuth middleware to prevent access to route
-
-/*  // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Opportunity }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  
-});
-*/
