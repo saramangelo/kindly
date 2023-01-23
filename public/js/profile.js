13b05ee5a36b362d7db7815f155ad0d65b36b0f5
saramangelo <saramangelo@gmail.com>
2023-01-23 00:17:33 -0800

    removed console logs

--- public/js/profile.js
+++ public/js/profile.js
@@ -53,6 +53,7 @@ const newFormHandler = async (event) => {
   }
 };
 
+
 // const delButtonHandler = async (event) => {
 //   if (event.target.hasAttribute('data-id')) {
 //     const id = event.target.getAttribute('data-id');
