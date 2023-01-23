0a87a5d1befd955eb9d41be581d1b875445def64
saramangelo <saramangelo@gmail.com>
2023-01-23 02:09:51 -0800

    successfully added DELETE route

--- public/js/profile.js
+++ public/js/profile.js
@@ -54,26 +54,26 @@ const newFormHandler = async (event) => {
 };
 
 
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-//     const response = await fetch(`/api/opportunities/${id}`, {
-//       method: 'DELETE',
-//     });
+    const response = await fetch(`/api/opportunities/${id}`, {
+      method: 'DELETE',
+    });
 
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to delete project');
-//     }
-//   }
-// };
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete opportunity');
+    }
+  }
+};
 
 document
   .querySelector('.new-opportunity-form')
   .addEventListener('submit', newFormHandler);
 
-// document
-//   .querySelector('.opportunity-list')
-//   .addEventListener('click', delButtonHandler);
+document
+  .querySelector('.opportunity-list')
+  .addEventListener('click', delButtonHandler);
